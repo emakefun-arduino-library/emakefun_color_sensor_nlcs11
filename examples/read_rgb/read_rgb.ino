5b2fe4ffd6d25af26ed6cb3489f7ec2a31ff7dcb
lt <1370582156@qq.com>
2024-09-23 14:56:12 +0800

    test

---
+++ examples/read_rgb/read_rgb.ino
@@ -0,0 +1,50 @@
+/**
+ * @example read_rgb.ino
+ */
+
+#include "color_sensor_nlcs11.h"
+
+#define INFINITE_LOOP_ON_FAILURE InfiniteLoopOnFailure(__FUNCTION__, __LINE__)
+
+namespace {
+uint8_t r, g, b;
+emakefun::ColorSensorNlcs11 g_color_sensor;
+void InfiniteLoopOnFailure(const char* function, const uint32_t line_number) {
+  Serial.println(String(F("entering an infinite loop due to failure in ")) +
+                 function + F(", at line number: ") + line_number);
+  while (true) {
+    yield();
+  }
+}
+
+}  // namespace
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("setup"));
+
+  Wire.begin();
+
+  Serial.println(String(F("color sensor lib version: ")) +
+                 emakefun::ColorSensorNlcs11::kVersionMajor + "." +
+                 emakefun::ColorSensorNlcs11::kVersionMinor + "." +
+                 emakefun::ColorSensorNlcs11::kVersionPatch);
+
+  const auto ret = g_color_sensor.Initialize();
+
+  if (emakefun::ColorSensorNlcs11::kOK == ret) {
+    Serial.println(F("color sensor initialization successful"));
+  } else {
+    Serial.println(String(F("color sensor device initialization failed: ")) +
+                   ret);
+    INFINITE_LOOP_ON_FAILURE;
+  }
+
+  Serial.println(F("setup successful"));
+}
+
+void loop() {
+  g_color_sensor.Rgb(&r, &g, &b);
+  Serial.println(String("r: ") + r + ", g: " + g + ", b: " + b);
+  delay(50);
+}
